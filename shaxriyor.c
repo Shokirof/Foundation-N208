d41528f272a415079e6060214db8654362982aa0
shaxriyor <shaxriyorvvvv@gmail.com>
2026-05-13 20:54:00 +0500

    Shaxriyor

--- shaxriyor.c
+++ shaxriyor.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 int main(){
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int n;
+   int cnt=0;
+    printf("n>>>> ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        if(n%2==0){
+            cnt++;
+        }
+    }
+          printf("%d",cnt);
     return 0;
 }
